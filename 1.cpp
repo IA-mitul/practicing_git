4f74a0f228192fbea49f64c2f273fa4162ab977c
IA-mitul <contact.istiuk@gmail.com>
2024-02-08 03:37:29 +0600

    add a programing file

---
+++ 1.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string name; //variable declaration
+    cout<<"Enter your name : "; //getting user input
+    cin>>name;
+    cout<<"Hello, "<<name; //printing output
+
+    return 0;
+}
